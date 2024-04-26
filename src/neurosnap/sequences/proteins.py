f1f5a6001b23494aa53edc566ef98071b3ebd6d2
Keaun A <kja.working@gmail.com>
2024-04-26 19:22:52 -0400

    add a combined file to msa generation

--- src/neurosnap/sequences/proteins.py
+++ src/neurosnap/sequences/proteins.py
@@ -241,8 +241,21 @@ def run_mmseqs2(seq, output, database="mmseqs2_uniref_env", use_filter=True, use
       with open(f"{output}/{fname}", "r") as fin:
         with open(f"{output}/{fname}.tmp", "w") as fout:
           for line in fin:
-            fout.write(line.replace("\x00",""))
+            fout.write(line.replace("\x00", ""))
       shutil.move(f"{output}/{fname}.tmp", f"{output}/{fname}")
+
+  # concatenate
+  with open(f"{output}/combined.a3m", "w") as fout:
+   with open(f"{output}/uniref.a3m") as f:
+    for line in f:
+      fout.write(line)
+
+   with open(f"{output}/bfd.mgnify30.metaeuk30.smag30.a3m") as f:
+    # skip first two lines
+    f.readline()
+    f.readline()
+    for line in f:
+      fout.write(line)
 
   # templates
   if use_templates:

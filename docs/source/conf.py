075b61220c444956eb29a19895beaa5d37399962
Mukund Mauji <mauji.mukund@yahoo.ca>
2024-10-30 13:04:05 -0400

    add typehints

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ extensions = [
   "sphinx.ext.autodoc",
   "sphinx.ext.napoleon",
   "sphinx.ext.viewcode",
-  # "sphinx_autodoc_typehints",
+  "sphinx_autodoc_typehints",
 ]
 
 templates_path = ["_templates"]

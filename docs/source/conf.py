149e6f93f2f8df0030a88e594b380e3e737d1b59
Keaun A <kja.working@gmail.com>
2024-11-19 16:08:47 -0500

    version increment

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ print(sys.executable)
 project = "neurosnap"
 copyright = "2024, Neurosnap Inc."
 author = "Keaun Amani"
-release = "0.0.67"
+release = "0.0.68"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration

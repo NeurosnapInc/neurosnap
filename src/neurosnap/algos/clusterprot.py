3460cde45ea14bf3d1de4a205bec0c7fe3c20da6
Keaun A <kja.working@gmail.com>
2025-06-14 15:56:30 +0200

    consistency

--- src/neurosnap/algos/clusterprot.py
+++ src/neurosnap/algos/clusterprot.py
@@ -99,7 +99,7 @@ def ClusterProt(
 
   # 2D projection and cluster it using DBSCAN
   if umap_n_neighbors == 0:
-    umap_n_neighbors = min(15, max(5, round(len(proteins_vects) * 0.01)))
+    umap_n_neighbors = min(15, max(5, round(len(proteins) * 0.01)))
   logger.debug(f"Creating 2D projection using UMAP (n_neighbors={umap_n_neighbors})")
   proj_2d = UMAP(n_components=2, n_neighbors=umap_n_neighbors, min_dist=0.04).fit_transform(proteins_vects)
 

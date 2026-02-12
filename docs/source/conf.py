0fdb77b5b3be52e5ae83fc9cc24ef77f5b44e868
Keaun A <kja.working@gmail.com>
2026-02-12 17:29:04 -0500

    changed doc themes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,6 +64,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "furo"  # NOTE: Default is "sphinx_rtd_theme"
+html_theme = "pydata_sphinx_theme"  # NOTE: Default is "sphinx_rtd_theme"
 html_static_path = ["_static"]
 html_favicon = "_static/favicon.ico"
